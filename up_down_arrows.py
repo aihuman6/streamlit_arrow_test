557a1d5fb4115090527a443f6babe32423023f9d
aihuman6 <99899987+aihuman6@users.noreply.github.com>
2022-10-18 14:31:00 +0100

    Update up_down_arrows.py

--- up_down_arrows.py
+++ up_down_arrows.py
@@ -14,4 +14,4 @@ html_df= df.to_html(escape=False, index=False)    #convert the df to html for be
 html_df = html_df.replace("<td>&amp;#8595;</td>","<td><font color = red>&#8595;</font></td>")    # Remove extra tags and added red colour to down arrow
 html_df = html_df.replace("<td>&amp;#8593;</td>","<td><font color = green>&#8593;</font></td>")    # Remove extra tags and added green colour to up arrow
 
-st.write(html_df, unsafe_allow_html=True)
+st.write(st.dataframe(html_df), unsafe_allow_html=True)
